7667264f2ffa24dc74e333b5ded72dd562d27844
Krzysztof Marczak <kmarczak@lsisoftware.pl>
2023-05-16 08:02:10 +0200

    CreateUpdateUser

---
+++ LSI.HOSP.AlaAllegro.Tests.Unit/Users/Handlers/CreateUpdateUserCommandHandlerTest.cs
@@ -0,0 +1,35 @@
+﻿using AutoFixture;
+using LSI.HOSP.AlaAllegro.Domain.Entities.Auctions;
+using LSI.HOSP.AlaAllegro.Domain.Entities.Users;
+using LSI.HOSP.AlaAllegro.Infrastructure;
+using LSI.HOSP.AlaAllegro.Infrastructure.DataAccess.Interfaces;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LSI.HOSP.AlaAllegro.Tests.Unit.Users.Handlers
+{
+    [TestFixture]
+    public class CreateUpdateUserCommandHandlerTest
+    {
+        private Mock<AppDbContext> _mockedDbContext;
+        private Mock<IRepository<User>> _userRepositoryMock;
+        private Fixture _fixture;
+
+        [SetUp]
+        public void Setup()
+        {
+            _fixture = new Fixture();
+            _userRepositoryMock = new Mock<IRepository<User>>();
+            _mockedDbContext = new Mock<AppDbContext>();
+            _fixture.Customize<User>(u => u.Without(a => a.Auctions).Without(po => po.PurchaseOffers));                        
+        }
+
+        [Test]
+
+    }
+}
---
+++ LSI.HOSP.AlaAllegro.Tests.Unit/Users/Validators/CreateUpdateUserCommandValidatorTest.cs
@@ -0,0 +1,22 @@
+﻿using LSI.HOSP.AlaAllegro.Application.Users.Commands;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LSI.HOSP.AlaAllegro.Tests.Unit.Users.Validators
+{
+    [TestFixture]
+    public class CreateUpdateUserCommandValidatorTest
+    {
+        private CreateUpdateUserCommandValidator _validator;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _validator = new CreateUpdateUserCommandValidator();
+        }
+    }
+}
